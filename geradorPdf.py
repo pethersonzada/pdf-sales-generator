279d94c0c405e5e81826a7f7af793b296e027b10
Miguel Petherson <147556184+pethersonzada@users.noreply.github.com>
2024-10-25 10:29:48 -0300

    Update geradorPdf.py

--- geradorPdf.py
+++ geradorPdf.py
@@ -5,7 +5,7 @@ from reportlab.pdfgen import canvas # Gerador de PDF.
 from reportlab.lib.pagesizes import A4 # Configuração de tamanho de página.
 from reportlab.pdfbase.ttfonts import TTFont # Fontes.
 
-os.makedirs("projetos-python/python-gerador-pdf", exist_ok=True) # Cria a pasta para o PDF, mas se a pasta já existir, não cria nada.
+os.makedirs("python-gerador-pdf", exist_ok=True) # Cria a pasta para o PDF, mas se a pasta já existir, não cria nada.
 
 nome = input("Nome do funcionário (Apenas primeiro nome) : ") # Input para pegar o nome do funcionário.
 quantidade_de_vendas = int(input("Quantidade de vendas: ")) # Input para pegar a quantidade de vendas do funcionário.
@@ -14,15 +14,15 @@ def avaliar_funcionario(quantidade_de_vendas): # Função para avaliar o rendime
     if quantidade_de_vendas >= 25:
         return "Sim!"
     elif quantidade_de_vendas < 25:
-        return"Não, mínimo de 25 vendas."
+        return "Não, mínimo de 25 vendas."
 
 avaliacao = avaliar_funcionario(quantidade_de_vendas)
 
 def mm2p(milimetros): # Função para converter milimetros em polegadas.
     return milimetros / 0.352777
 
-cnv = canvas.Canvas("projetos-python/python-gerador-pdf/Meu primeiro Pdf.pdf") # Cria o PDF.
-cnv.drawImage("projetos-python/python-gerador-pdf/plano-de-fundo.png", 0, 0, width=A4[0], height=A4[1]) # Insere o plano de fundo do PDF.
+cnv = canvas.Canvas("python-gerador-pdf/avaliacao-do-funcionario.pdf") # Cria o PDF.
+cnv.drawImage("python-gerador-pdf/plano-de-fundo.png", 0, 0, width=A4[0], height=A4[1]) # Insere o plano de fundo do PDF.
 
 cnv.drawString(mm2p(153), mm2p(242.1), nome) # Insere o nome do funcionário no PDF.
 cnv.drawString(mm2p(156), mm2p(180), str(quantidade_de_vendas)) # Insere a quantidade de vendas do funcionário no PDF.
